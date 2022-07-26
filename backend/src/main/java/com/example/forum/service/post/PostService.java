0280799457dcc7a1dd246d3b2a533c3405e8773f
Nirav Gusai <gusainirav1@gmail.com>
2022-07-26 11:21:43 +0530

    Merge Conflicts resolve in postService

--- backend/src/main/java/com/example/forum/service/post/PostService.java
+++ backend/src/main/java/com/example/forum/service/post/PostService.java
@@ -10,11 +10,7 @@ import com.example.forum.dto.Post;
 public interface PostService {
     List<Post>getAllPost();
     
-<<<<<<< HEAD:backend/src/main/java/com/example/forum/service/PostService.java
-    Post createPost(Post post);
-=======
     Post createPost(Post post) throws IOException;
->>>>>>> 32d3fa4e90f63404f3ad0c01f39092b0674466f4:backend/src/main/java/com/example/forum/service/post/PostService.java
 
     Post updatePost(String id,Post post) throws Exception;
 
