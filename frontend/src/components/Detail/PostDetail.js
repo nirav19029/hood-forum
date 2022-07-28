761226dec80436e19e0aa70e636437bdb34b1e93
Nirav Gusai <gusainirav1@gmail.com>
2022-07-28 18:14:19 +0530

    added style to custom card

--- frontend/src/components/Detail/PostDetail.js
+++ frontend/src/components/Detail/PostDetail.js
@@ -4,12 +4,12 @@ import Card from "../Card/Card";
 
 const PostDetail = (props) => {
   const data = require("./../../data/post.json");
-  console.log(props.active);
+  //   console.log(props.active);
   const requirePost = data.filter((value) => value._id === props.active);
-  console.log(requirePost);
+  //   console.log(requirePost);
   return (
     <div className="detail-container">
-      <Card value={requirePost[0]} />
+      <Card value={requirePost[0]} className="custom" />
     </div>
   );
 };
