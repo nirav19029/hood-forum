7613b43286bc8dde69413f0c4aa8abc454a2e193
Divyanshu Raj <divyanshuraj5050@gmail.com>
2022-08-01 17:55:02 +0530

    add user save functionality + error handling in comment / create

--- backend/src/main/java/com/example/forum/dto/Comment.java
+++ backend/src/main/java/com/example/forum/dto/Comment.java
@@ -3,84 +3,98 @@ package com.example.forum.dto;
 import java.util.Date;
 
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
 public class Comment {
     @Id
     @JsonIgnore
     @JsonProperty("_id")
     private String commentId;
 
+    @NotNull
+    @NotEmpty
     private String postId;
+    @NotNull
+    @NotEmpty
     private String userId;
 
+    @NotNull
+    @NotEmpty
     private String comment;
 
     private Date timestamp;
 
     private Date updatedOn;
 
-    //NoArgs
-    public Comment(){
-        super();
-    }
-    //AllArg
-    public Comment(String commentId, String postId, String userId, String comment,Date timestamp,Date updatedOn){
-        super();
-        this.commentId = commentId;
-        this.postId = postId;
-        this.userId = userId;
-        this.comment = comment;
-        this.timestamp = timestamp;
-        this.updatedOn = updatedOn; 
-    }
+//     //NoArgs
+//     public Comment(){
+//         super();
+//     }
+//     //AllArg
+//     public Comment(String commentId, String postId, String userId, String comment,Date timestamp,Date updatedOn){
+//         super();
+//         this.commentId = commentId;
+//         this.postId = postId;
+//         this.userId = userId;
+//         this.comment = comment;
+//         this.timestamp = timestamp;
+//         this.updatedOn = updatedOn; 
+//     }
 
-    public String getCommentId(){
-        return commentId;
-    }
+//     public String getCommentId(){
+//         return commentId;
+//     }
 
-    public void setCommentId(String commentId){
-        this.commentId = commentId;
-    }
+//     public void setCommentId(String commentId){
+//         this.commentId = commentId;
+//     }
 
-    public String getPostId() {
-		return postId;
-	}
+//     public String getPostId() {
+// 		return postId;
+// 	}
 
-	public void setPostId(String postId) {
-		this.postId = postId;
-	}
+// 	public void setPostId(String postId) {
+// 		this.postId = postId;
+// 	}
 
-    public String getUserId() {
-		return userId;
-	}
+//     public String getUserId() {
+// 		return userId;
+// 	}
 
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
+// 	public void setUserId(String userId) {
+// 		this.userId = userId;
+// 	}
 
-    public String getComment() {
-		return comment;
-	}
+//     public String getComment() {
+// 		return comment;
+// 	}
 
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
+// 	public void setComment(String comment) {
+// 		this.comment = comment;
+// 	}
 
-    public Date getTimestamp() {
-		return timestamp;
-	}
+//     public Date getTimestamp() {
+// 		return timestamp;
+// 	}
 
-	public void setTimestamp(Date timestamp) {
-		this.timestamp = timestamp;
-	}
+// 	public void setTimestamp(Date timestamp) {
+// 		this.timestamp = timestamp;
+// 	}
 
-    public Date getUpdatedOn(){
-        return updatedOn;
-    }
-    public void setUpdatedOn(Date updatedOn){
-        this.updatedOn = updatedOn;
-    }
+//     public Date getUpdatedOn(){
+//         return updatedOn;
+//     }
+//     public void setUpdatedOn(Date updatedOn){
+//         this.updatedOn = updatedOn;
+//     }
 }
