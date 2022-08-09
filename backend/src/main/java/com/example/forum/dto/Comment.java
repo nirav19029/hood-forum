7b63f019e48c5d2131c9072a38f259ffb94949bd
pp-nb <prakhar.pandey@nobroker.in>
2022-08-09 13:30:44 +0530

    corrected postId and comment date

--- backend/src/main/java/com/example/forum/dto/Comment.java
+++ backend/src/main/java/com/example/forum/dto/Comment.java
@@ -28,70 +28,9 @@ public class Comment {
     @NotNull
     @NotEmpty
     private String comment;
-
+    
     private Date timestamp;
 
     private Date updatedOn;
 
-//     //NoArgs
-//     public Comment(){
-//         super();
-//     }
-//     //AllArg
-//     public Comment(String commentId, String postId, String userId, String comment,Date timestamp,Date updatedOn){
-//         super();
-//         this.commentId = commentId;
-//         this.postId = postId;
-//         this.userId = userId;
-//         this.comment = comment;
-//         this.timestamp = timestamp;
-//         this.updatedOn = updatedOn; 
-//     }
-
-//     public String getCommentId(){
-//         return commentId;
-//     }
-
-//     public void setCommentId(String commentId){
-//         this.commentId = commentId;
-//     }
-
-//     public String getPostId() {
-// 		return postId;
-// 	}
-
-// 	public void setPostId(String postId) {
-// 		this.postId = postId;
-// 	}
-
-//     public String getUserId() {
-// 		return userId;
-// 	}
-
-// 	public void setUserId(String userId) {
-// 		this.userId = userId;
-// 	}
-
-//     public String getComment() {
-// 		return comment;
-// 	}
-
-// 	public void setComment(String comment) {
-// 		this.comment = comment;
-// 	}
-
-//     public Date getTimestamp() {
-// 		return timestamp;
-// 	}
-
-// 	public void setTimestamp(Date timestamp) {
-// 		this.timestamp = timestamp;
-// 	}
-
-//     public Date getUpdatedOn(){
-//         return updatedOn;
-//     }
-//     public void setUpdatedOn(Date updatedOn){
-//         this.updatedOn = updatedOn;
-//     }
 }
