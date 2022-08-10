6af6c90f1499c67fa4609543981c9c8da9b72b12
Nirav Gusai <gusainirav1@gmail.com>
2022-08-10 11:12:51 +0530

    image upload logic implemented in modal

--- frontend/src/components/Detail/PostDetail.js
+++ frontend/src/components/Detail/PostDetail.js
@@ -1,43 +1,38 @@
-import React, { useEffect, useState,useContext } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import "./PostDetail.css";
 import Card from "../Card/Card";
 import { Tab } from "@mui/material";
-import Chip from '@mui/material/Chip';
-import DeleteIcon from '@mui/icons-material/Delete';
-import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import Chip from "@mui/material/Chip";
+import DeleteIcon from "@mui/icons-material/Delete";
+import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
 import Stack from "@mui/material/Stack";
 // import userContext from "../../Contexts/userContext";
 
 const PostDetail = (props) => {
-  console.log({props})
   let [comment, setComment] = useState([]); // for viewing comment
 
   // const user = useContext(userContext);
   let user = {
     id: "123",
-    name: "nproker"
-  }
-  
+    name: "nproker",
+  };
+
   let loggedInUserId = user.id;
-
-
 
   ///-------------commentView--------------------------------------
 
   useEffect(() => {
-  
-    let url = "http://192.168.5.128:8080/forum/v1/post/comment/62e8bd8a3160e9725a0fc7ac";
+    let url =
+      "http://192.168.5.128:8080/forum/v1/post/comment/62e8bd8a3160e9725a0fc7ac";
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
         setComment([...data]);
-      })
-  },[]);
+      });
+  }, []);
   //------------------------------------------------------------------------------------
 
-  const postComment = () => {
-
-  }
+  const postComment = () => {};
 
   //----------------deleting a comment---------------------------
   const handleDelete = (id) => {
@@ -45,97 +40,72 @@ const PostDetail = (props) => {
     console.log(updateCommentList.length);
     updateCommentList = updateCommentList.filter((e) => {
       return e.id !== id;
-    })
+    });
     console.log(updateCommentList.length + "==>final");
-    setComment([...updateCommentList])
-    console.info('You clicked the delete icon.');
+    setComment([...updateCommentList]);
+    console.info("You clicked the delete icon.");
     console.log(props + "props.active");
   };
   //--------------------------------------------------------------
 
-  const handleClick = event => {
-    event.currentTarget.classList.toggle('afterClick');
+  const handleClick = (event) => {
+    event.currentTarget.classList.toggle("afterClick");
     console.log(setCurrentState);
-  }
+  };
 
   return (
     <div className="detail-container">
       <Card value={props.postData} className="custom" />
       <br></br>
-      
-        {comment.length>0 ? (
-          <div id="comments-view">
-          {comment.map((comm) => {
-          return <div id="singleComment">
-            <div className="commentBody">
 
+      {comment.length > 0 ? (
+        <div id="comments-view">
+          {comment.map((comm) => {
+            return (
+              <div id="singleComment">
+                <div className="commentBody">
                   {loggedInUserId == props.postData.userId ? (
                     <Stack spacing={1} direction="row">
-                  <h4>{comm.name}</h4>
-                  <Chip
-                    onClick = {() => handleDelete(comm.id)}
-                    onDelete={() => handleDelete(comm.id)}
-                    label="Delete"
-                    deleteIcon={<DeleteIcon />}
-                    size="small"
-                    variant="filled"
-                  />
-                  <Chip
-                    onClick={() => handleClick}
-                    label="Like"
-                    icon={<ThumbUpOffAltIcon />}
-                    variant="filled"
-                    size="small"
-                  /> </Stack>
+                      <h4>{comm.name}</h4>
+                      <Chip
+                        onClick={() => handleDelete(comm.id)}
+                        onDelete={() => handleDelete(comm.id)}
+                        label="Delete"
+                        deleteIcon={<DeleteIcon />}
+                        size="small"
+                        variant="filled"
+                      />
+                      <Chip
+                        onClick={() => handleClick}
+                        label="Like"
+                        icon={<ThumbUpOffAltIcon />}
+                        variant="filled"
+                        size="small"
+                      />{" "}
+                    </Stack>
                   ) : (
                     <Stack spacing={1} direction="row">
-                  <h4>{comm.name}</h4>
-                  <Chip
-                    onClick={() => handleClick}
-                    label="Like"
-                    icon={<ThumbUpOffAltIcon />}
-                    variant="filled"
-                    size="small"
-                  /> </Stack>
+                      <h4>{comm.name}</h4>
+                      <Chip
+                        onClick={() => handleClick}
+                        label="Like"
+                        icon={<ThumbUpOffAltIcon />}
+                        variant="filled"
+                        size="small"
+                      />{" "}
+                    </Stack>
                   )}
-              {comm.body}</div>
-          </div>
-        })
-        }
+                  {comm.body}
+                </div>
+              </div>
+            );
+          })}
         </div>
-        ): (<div></div>)}
-      
-
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
 
-
-
-
-
 export default PostDetail;
-
-
-
-//   console.log(requirePost);
-
-
-  // const viewAllComments = () => {
-  // let url = "https://jsonplaceholder.typicode.com/comments";
-  // fetch(url)
-  // .then((res) => res.json())
-  // .then((data) => {
-  //   let output = "<h2>Comments</h2>";
-  //   console.log(data);
-  //   data.forEach(element => {
-  //     output += `<div>
-
-  //              <p> ${element.body} </p>
-  //              </div>
-  //              <div style="border:1px solid black"></div>
-  //              `;
-  //   });
-  //   document.getElementById('comments-view').innerHTML = output;
-  // })
-  // }
--- frontend/src/components/Modal/AddFormModal.js
+++ frontend/src/components/Modal/AddFormModal.js
@@ -1,10 +1,13 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Modal from "@mui/material/Modal";
 import AddAPhotoIcon from "@mui/icons-material/AddAPhoto";
 import PostAddIcon from "@mui/icons-material/PostAdd";
 import "./AddFormModal.css";
+import axios from "axios";
+
+const api = "http://192.168.5.128:8080/forum/v1/createPost";
 
 const style = {
   position: "relative",
@@ -20,9 +23,29 @@ const style = {
 };
 
 export default function BasicModal() {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const [topic, setTopic] = useState("");
+  const [description, setDescription] = useState("");
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleTopicChange = (event) => {
+    const inputTopic = event.target.value;
+    // console.log(inputTopic);
+    setTopic(inputTopic);
+  };
+
+  const handleDescriptionChange = (event) => {
+    const inputDescription = event.target.value;
+    // console.log(inputDescription);
+    setDescription(inputDescription);
+  };
+
+  const handleImgUpload = (event) => {
+    // console.log(event.target.files[0]);
+    setSelectedImage(event.target.files[0]);
+  };
 
   return (
     <div className="main-modal">
@@ -45,7 +68,13 @@ export default function BasicModal() {
             <label style={{ display: "block" }} for="topic">
               <b>Enter your topic to discuss :</b>
             </label>
-            <textarea rows="2" required style={{ width: "100%" }} id="topic" />
+            <textarea
+              rows="2"
+              required
+              style={{ width: "100%" }}
+              id="topic"
+              onChange={(e) => handleTopicChange(e)}
+            />
           </div>
 
           <div style={{ fontSize: "120%" }}>
@@ -59,28 +88,52 @@ export default function BasicModal() {
                 width: "100%",
               }}
               id="description"
+              onChange={(e) => handleDescriptionChange(e)}
             />
           </div>
 
-          <div>
+          {/* <div>
             <Button variant="contained" component="label">
               <AddAPhotoIcon style={{ marginRight: "10px" }} />
               Add Photo
               <input hidden accept="image/*" multiple type="file" />
             </Button>
-          </div>
+          </div> */}
+          {selectedImage && (
+            <div>
+              <img
+                alt="not fount"
+                width={"200px"}
+                src={URL.createObjectURL(selectedImage)}
+              />
+              <br />
+              <button onClick={() => setSelectedImage(null)}>Remove</button>
+            </div>
+          )}
+          <Button variant="contained" component="label">
+            <AddAPhotoIcon style={{ marginRight: "10px" }} />
+            Add Photo
+            <input
+              type="file"
+              name="myImage"
+              hidden
+              accept="image/*"
+              multiple
+              onChange={(e) => handleImgUpload(e)}
+            />
+          </Button>
 
           <div className="finalButton">
-            <Button variant="contained" color="success">
-              Post
+            <Button variant="contained" color="error" onClick={handleClose}>
+              Cancel
             </Button>
+
             <Button
               variant="contained"
-              color="error"
+              color="success"
               style={{ marginLeft: "1rem" }}
-              onClick={handleClose}
             >
-              Cancel
+              Post
             </Button>
           </div>
         </Box>
